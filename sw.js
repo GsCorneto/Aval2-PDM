6778d678317daca3d0227d4d11fe055cd7710747
jppegarare <jppegarare18@gmail.com>
2024-11-09 10:32:25 -0400

    pequenas alterações

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 const cacheName = "pwa-receitas" ;
 const arquivosSalvos = ["/", "/index", "/styles.css", "/main.js", 
-                    "/routes/carnes", "/routes/doces", "/routes/massas",
-                    "/images/bife.jpg", "/images/bolocarne.jpg", "/images/brigadeiro.jpg", 
-                    "/images/brownie.jpg", "/images/macarrao.jpg", "/images/maracuja.jpg",
-                    "/images/panqueca.jpg", "/images/pastel.jpg", "/images/strogonoff.jpg"];
+                        "/images"];
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
